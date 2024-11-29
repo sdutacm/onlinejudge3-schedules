5dabba4ced8e83328915fbbf98464ce3e3d03e86
bLue <tbdblue@gmail.com>
2024-11-29 23:03:09 +0800

    chore: update

--- configs/oj3-prerender.prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  problems: '/var/www/onlinejudge3/prerender/problems/',
-  topics: '/var/www/onlinejudge3/prerender/topics/',
-  posts: '/var/www/onlinejudge3/prerender/posts/',
-};
--- configs/oj3-sitemap.prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  problems: '/var/www/onlinejudge3/sitemaps/problems.txt',
-  topics: '/var/www/onlinejudge3/sitemaps/topics.txt',
-  posts: '/var/www/onlinejudge3/sitemaps/posts.txt',
-};
